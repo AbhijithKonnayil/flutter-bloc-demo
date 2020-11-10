280d03840f4806c77604c8cd271494f6d50c7d4d
Fansa2000 <fansahfs@gmail.com>
2020-11-10 19:55:04 +0530

    added heba.bloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:demo/abhi_bloc/abhi_bloc.dart';
 import 'package:demo/home_bloc/home_bloc.dart';
 import 'package:demo/home_page.dart';
 import 'package:demo/pages/abhi_page.dart';
+import 'package:demo/pages/heba_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
+
+import 'heba_bloc/heba_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,10 +27,13 @@ class MyApp extends StatelessWidget {
           providers: [
             BlocProvider<HomeBloc>(
                 create: (BuildContext context) => HomeBloc()),
-            BlocProvider<AbhiBloc>(create: (BuildContext context) => AbhiBloc())
+            BlocProvider<AbhiBloc>(create: (BuildContext context) => AbhiBloc()),
+            BlocProvider<HebaBloc>(create: (BuildContext context) => HebaBloc())
           ],
-          child: PageView(
-            children: [HomePage(), AbhiPage()],
+          child: SafeArea(
+            child: PageView(
+              children: [HomePage(), AbhiPage(),HebaPage()],
+            ),
           )),
     );
   }
