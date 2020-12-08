66da9bfc8735f2957c7ecb99de6012b4d3eef55e
JobinBiju <jobinbiju9090@gmail.com>
2020-12-08 14:38:15 +0530

    Customer Data Model

---
+++ lib/models/jobin_customer_model.dart
@@ -0,0 +1,9 @@
+class Customer {
+  int id;
+  String name;
+  String email;
+  double phoneNo;
+  Map<String, dynamic> address;
+
+  Customer(this.id, this.name, this.address, this.phoneNo, this.email);
+}
