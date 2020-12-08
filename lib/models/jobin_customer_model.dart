c36d581b6ce836a1194545c2850f79c20363d956
JobinBiju <jobinbiju9090@gmail.com>
2020-12-08 19:10:30 +0530

    Customer Data Model

--- lib/models/jobin_customer_model.dart
+++ lib/models/jobin_customer_model.dart
@@ -6,4 +6,19 @@ class Customer {
   Map<String, dynamic> address;
 
   Customer(this.id, this.name, this.address, this.phoneNo, this.email);
+
+  Customer.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        email = json['email'],
+        phoneNo = json['phoneNo'],
+        address = json['address'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'email': email,
+        'phoneNo': phoneNo,
+        'address': address,
+      };
 }
